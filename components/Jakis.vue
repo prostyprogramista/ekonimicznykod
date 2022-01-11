9aaf76b24844c755bf004b217406fb9bc1a79ced
Andrzej Kwiatkowski <akwiatkowski.tech@outlook.com>
2022-01-11 18:19:50 +0100

    Dynamic routing for content markdown files v3

---
+++ components/Jakis.vue
@@ -0,0 +1,13 @@
+<template>
+    <nuxt-content :document="article" />
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const article = await $content('blog/me').fetch();
+
+    return { article };
+  },
+};
+</script>
--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,15 +1,28 @@
 <template>
-  <article>
+  <div>
+    <pre> {{ article }} </pre>
+    <p>body: {{ body }}</p>
+    <p>slug: {{ slug }}</p>
+    <p>last: {{ last }}</p>
+    <p>createdAt: {{ created }}</p>
+    <p>dir: {{ dir }}</p>
+    <p>path: {{ path }}</p>
     <nuxt-content :document="article" />
-  </article>
+  </div>
 </template>
 
 <script>
 export default {
   async asyncData({ $content, params }) {
-    const article = await $content('blog', params.slug).fetch();
+    const body = params.body;
+    const slug = params.slug;
+    const last = params.updatedAt;
+    const created = params.createdAt;
+    const dir = params.dir;
+    const path = params.path;
+    const article = await $content("blog/me").fetch();
 
-    return { article };
+    return { body, slug, last, created, dir, path, article };
   },
 };
 </script>
