8bc4c44f900ee3b63a0854127d5a054543534bff
Andrzej Kwiatkowski <akwiatkowski.tech@outlook.com>
2022-01-11 17:29:13 +0100

    Dynamic routing for content markdown files v3

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -7,7 +7,7 @@
 <script>
 export default {
   async asyncData({ $content, params }) {
-    const article = await $content("blog", params.slug).fetch();
+    const article = await $content('blog', params.slug).fetch();
 
     return { article };
   },
