d9f30a17c523880f61c8cb7c01adbd5a86fb5f10
Andrzej Kwiatkowski <akwiatkowski.tech@outlook.com>
2022-01-11 21:14:53 +0100

    Created new script

--- script1.js
+++ script1.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 
-fs.copyFile(`${__dirname}\\pages\\me.vue`, `${__dirname}\\pages\\blog\\mojblog.vue`, (err) => {
+fs.copyFile(`${__dirname}/pages/me.vue`, `${__dirname}/pages/blog/mojblog.vue`, (err) => {
     if(err) throw err;
 
     console.log("File was copied")
