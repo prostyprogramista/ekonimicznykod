3abff683122eee55f5c5d8050518ed73b8461ae9
Andrzej Kwiatkowski <akwiatkowski.tech@outlook.com>
2022-01-12 21:04:43 +0100

    Modify template

--- pages/database/sql_vs_nosql.vue
+++ pages/database/sql_vs_nosql.vue
@@ -1,18 +1,15 @@
 <template>
   <div>
-    <pre> {{ article }} </pre>
-    <h1> To jest SLUG: {{ slug }} </h1>
     <nuxt-content :document="article" />
   </div>
 </template>
 
 <script>
 export default {
-  async asyncData({ $content, params }) {
-    const slug = params.slug
+  async asyncData({ $content }) {
     const article = await $content('database/sql_vs_nosql').fetch();
 
-    return { slug, article };
+    return { article };
   },
 };
 </script>
