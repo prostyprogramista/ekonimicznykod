89fccb29ccd8f8d89bfcaa906863f3a0ba8b85b5
prostyprogramista <prostyprogramista@users.noreply.github.com>
2022-03-19 14:16:22 +0000

    deploy: f3901a4ecfa45622559d4b6ddccc6c2216b6c96e

--- _nuxt/0924252.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{292:function(t,e,n){"use strict";n.r(e);var c=n(23),component=Object(c.a)({},(function(){var t=this,e=t.$createElement;return(t._self._c||e)("section",{staticClass:"my-section"},[t._t("default")],2)}),[],!1,null,null,null);e.default=component.exports}}]);
