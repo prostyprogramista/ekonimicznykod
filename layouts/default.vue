20fb3a6ed6e8268cfb21c82ef42acca84b64590b
Andrzej Kwiatkowski <akwiatkowski.tech@outlook.com>
2022-07-02 11:00:03 +0200

    Added parametrization to latest post

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,5 +3,23 @@
         <NavigationBar />
         <Nuxt/>
         <Footer/>
+      <div v-for="article of articles" :key="article">
+        {{ article.title }}
+      </div>
     </div>
 </template>
+
+<script>
+export default {
+  async asyncData({ $content }) {
+    const articles = await $content({ deep: true })
+      .only(["title"])
+      .sortBy("createdAt", "desc")
+      .fetch();
+
+      console.log(articles)
+
+    return { articles };
+  },
+};
+</script>
