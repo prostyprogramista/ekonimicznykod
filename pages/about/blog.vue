654c85b040bb7b87ecd752f14ec03e96b661e296
Andrzej Kwiatkowski <akwiatkowski.tech@outlook.com>
2022-07-31 10:59:54 +0200

    About blog text changes.

--- pages/about/blog.vue
+++ pages/about/blog.vue
@@ -5,7 +5,17 @@
           <p>
             Mówią „nowy rok, nowy ja”, wierzysz w to? Ja też nie, a mimo to blog, który właśnie czytasz, powstał na przełomie lat 2021/2022 – wtedy właśnie zacząłem tworzyć jego 
             pierwsze szkice. Pewnie można by to nazwać zbiegiem okoliczności lub przypadkiem, jednak może faktycznie ten mistyczny czas nowego roku bardziej prowokuje do działania – 
-            nie wiem, to nieistotne. Istotne jest jednak to, że blog w końcu ruszył! :)
+            nie wiem, to nieistotne. Istotne jest jednak to, że pierwsze prace nad blogiem w końcu ruszył! :)
+          </p>
+          <p>
+            Rok 2022 był kluczowy pod względem prac związanych z tworzeniem platformy bloga. Osobiście lubię mieć kontrolę nad każdym aspektem tworzonych przezemnie rozwiązań. Z tego 
+            włąśnie powodu zapadła decyzja o tworzenia blog'a samodzielnie od zera. Początkowo testowałem kilka gotowych rozwiązań na rynku, jednak po pierwszych próbach tworzenia bloga 
+            w popularnych rozwiązaniach, poczułem ich ograniczenia, co przełożyło się na decyzję o samodzielnym zaprogramowaniu całego silnika do tworzenia nowych artykułów. Decyzja 
+            ta była pewnie podyktowana tym, że jednak na codzień programuję, więc stworzenie własnego rozwiązanie nie było dla mnie większym wyzwaniem, a elastyczność jaką uzyskałem 
+            dzięki takiemu podejściu moim zdaniem rekompensuje włożony w to czas.
+          </p>
+          <p>
+            Technologie jakie wybrałem to...
           </p>
           <p>
             Główną ideą podczas zakładania bloga była chęć głębszej i pełniejszej eksploracji poznawanych przeze mnie zagadnień i technologi. Rezultaty będę publikował w formie 
